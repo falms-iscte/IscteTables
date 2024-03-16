1ca68a32402699a3f10af30ddcf2300bdce20d5a
falms-iscte <163667488+falms-iscte@users.noreply.github.com>
2024-03-16 17:21:23 +0000

    Create Exemplo.java
    continuação da ficha de aula prática 2 sobre git t github

---
+++ Projeto/src/Projeto/Exemplo.java
@@ -0,0 +1,11 @@
+package Projeto;
+
+
+
+
+public class Exemplo {
+    public static void main(String[] args) {
+        String mensagem = "Este é um exemplo de uma classe Java!";
+        System.out.println(mensagem);
+    }
+}
