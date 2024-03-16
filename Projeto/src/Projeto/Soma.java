c970e282f56e92d6b86878094d134e9843ee5de3
falms-iscte <163667488+falms-iscte@users.noreply.github.com>
2024-03-16 17:07:05 +0000

    Create Soma.java
    ainda não é nada sobre projeto, apenas aula prática 2

---
+++ Projeto/src/Projeto/Soma.java
@@ -0,0 +1,14 @@
+package Projeto;
+
+public class Soma {
+
+	
+	    public static int somar(int a, int b) {
+	        return a + b;
+	    }
+
+	    public static void main(String[] args) {
+	        int resultado = somar(5, 3);
+	        System.out.println("A soma é: " + resultado);
+	    }
+}
